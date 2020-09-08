f61d83fd2f5fb45e8da40103b216300d72a78fbf
Kaliztro <g.lopes1999@hotmail.com>
2020-09-08 09:08:18 -0300

    att guildCreate

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,5 +2,5 @@ module.exports = (client, guild) => {
 
     let user = client.users.cache.get('459559578648969227')
   
-    user.send("Eu acabei de entrar no servidor **"+ guild.name + "**");
+    user.send("Eu acabei de entrar no servidor **"+ guild.name + "** " + guild.id );
   }
