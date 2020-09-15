e5f5aec9791468f5acf4e6795f6009412c375b0a
Kaliztro <g.lopes1999@hotmail.com>
2020-09-15 09:43:45 -0300

    adicionado um timer para pingar um outro site

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,16 @@ module.exports = async (client) => {
     client.user
         .setStatus("online") // idle, dnd, online, invisible
         .catch(console.error);
-        console.log(`Eu estou online agora, meu nome é ${client.user.username}. Há ${client.users.cache.size} usuario(s) em ${client.guilds.cache.size} servidor(es)!`)
+        console.log(`Eu estou online agora, meu nome é ${client.user.username}. Há ${client.users.cache.size} usuario(s) em ${client.guilds.cache.size} servidor(es)!`);
+      
+        
+//fica pingando um site 
+/*
+const fetch = require('node-fetch');
+setInterval(function() {
+fetch('https://lucy.guilhermemagal3.repl.co');
+}, 1000 * 60 * 1); //tempo definido para 1 minuto
+*/
+
+
   };
