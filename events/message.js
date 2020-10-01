65b7a5e91e6fff39761aa01264f2542e57485da8
Kaliztro <g.lopes1999@hotmail.com>
2020-10-01 15:31:42 -0300

    corrigido erro em q o bot n respondia a mençao

--- events/message.js
+++ events/message.js
@@ -1,11 +1,16 @@
+const config = require("../config.json")
+
 module.exports = async (client, message) => {
 
-//bot envia uma mensagem quando mencionado
+    const emoj = client.guilds.cache.get("545386837846523905");
+    const emoji = emoj.emojis.cache.find(emoji => emoji.name === "cagliostro");
 
-const menção = message.mentions.users.first()
-if(menção){
-if(menção.id === `${client.user.id}`) return message.channel.send(`Meu prefixo é:  **${config.prefix}**   Para mais informações utilize: **${config.prefix}ajuda**`)
-};
+
+
+    const menção = message.mentions.users.first()
+    if(menção){
+    if(menção.id === `${client.user.id}`) return message.channel.send(`${emoji}   Olá ${message.member.user}! Meu prefixo é:  **${config.prefix}**   Para mais informações utilize: **${config.prefix}ajuda**`)
+    };
 
 
 
