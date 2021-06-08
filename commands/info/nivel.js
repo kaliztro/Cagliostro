05e38d49c481ae20eb3b11a1e79f850df43e77c8
Kaliztro <g.lopes1999@hotmail.com>
2021-06-07 21:52:30 -0300

    corrigido erro que mostrava o seu avatar avatar ao consultar lvl de outra pessoa

--- commands/info/nivel.js
+++ commands/info/nivel.js
@@ -17,7 +17,7 @@ module.exports = {
                 const levell = snap.val().level
                 const xpp = snap.val().xp
                 const nextLevel = snap.val().nextLevel
-                let avatarr = message.author.avatarURL({ dynamic: true, format: 'png', size: 1024 });
+                let avatarr = user.avatarURL({ dynamic: true, format: 'png', size: 1024 });
                 let lEmbed = new Discord.MessageEmbed()
                     .setThumbnail(avatarr)
                     .setTitle("``Nível``")
