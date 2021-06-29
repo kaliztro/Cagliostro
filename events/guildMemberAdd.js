732dc30cb0964a4ed89f0de35b30c12853386dbf
Kaliztro <g.lopes1999@hotmail.com>
2021-06-29 01:09:07 -0300

    corrigido bug que nao mostrava o prefixo na mensagem de boas vindas

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,6 +3,8 @@ const firebase = require('firebase');
 const database = firebase.database();
 const config = require('../config.json')
 
+const c = require("../index")
+
 module.exports = async (client, member, message, guild) => {
 
   database.ref(`Servidor/${member.guild.id}/Entrada`)
@@ -11,7 +13,6 @@ module.exports = async (client, member, message, guild) => {
     if (snap.val() == null) return;
           
     const Mcanal = snap.val().canal
-    const Mserver = snap.val().server
 
     if (!member.user.bot){
 
@@ -20,7 +21,7 @@ module.exports = async (client, member, message, guild) => {
       .setAuthor(member.user.tag, member.user.displayAvatarURL())
       .setTitle(`Boas-vindas`)
       .setImage("https://i.imgur.com/QzfNwIE.gif")
-      .setDescription(`**${member.user}**, bem-vindo(a) aos** ${member.guild.name} **! :heart: \n Qualquer duvida sobre os comandos é so digitar **!ajuda**`)
+      .setDescription(`**${member.user}**, bem-vindo(a) aos** ${member.guild.name} **! :heart: \n Qualquer duvida sobre os comandos é so digitar **${c.prefix}ajuda**`)
       .setThumbnail(member.user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }))
       .setFooter(' ID do usuario: ' + member.user.id)
       .setTimestamp();
