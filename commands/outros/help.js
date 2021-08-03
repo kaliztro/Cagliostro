98ff6169b29e0646b5cd589195cbfade5135a382
Kaliztro <g.lopes1999@hotmail.com>
2021-08-03 18:41:34 -0300

    comando 2

---
+++ commands/outros/help.js
@@ -0,0 +1,57 @@
+const { MessageEmbed } = require("discord.js");
+const config = require("../../config.json");
+
+module.exports = {
+  name: "help",
+  description:"Get list of all command and even get to know every command detials",
+  usage: "help <cmd>",
+  category: "info",
+  run: async (client, message, args) => {
+    if (args[0]) {
+      const command = await client.commands.get(args[0]);
+
+      if (!command) {
+        return message.channel.send("Comando desconhecido: " + args[0]);
+      }
+
+      let embed = new MessageEmbed()
+        .setAuthor(command.name, client.user.displayAvatarURL())
+        .addField("Descrição", command.description || "Não definido :(")
+        .addField("Como usar:", "`" + command.usage + "`" || "Não definido")
+        .setThumbnail(client.user.displayAvatarURL())
+        .setColor(config.cor)
+        .setFooter(client.user.username, client.user.displayAvatarURL());
+
+      return message.channel.send(embed);
+    } else {
+      const commands = await client.commands;
+
+      let emx = new MessageEmbed()
+        .setDescription("**para saber como usar algum comando digite !ajuda e o nome do comando 😊")
+        .setColor(config.cor)
+        .setFooter(client.user.username, client.user.displayAvatarURL())
+        .setThumbnail(client.user.displayAvatarURL());
+
+      let com = {};
+      for (let comm of commands.array()) {
+        let category = comm.category || "Desconhecido";
+        let name = comm.name;
+
+        if (!com[category]) {
+          com[category] = [];
+        }
+        com[category].push(name);
+      }
+
+      for(const [key, value] of Object.entries(com)) {
+        let category = key;
+
+        let desc = "`" + value.join("`, `") + "`";
+
+        emx.addField(`${category.toUpperCase()}[${value.length}]`, desc);
+      }
+
+      return message.channel.send(emx);
+    }
+  }
+};
