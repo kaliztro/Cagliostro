2066635838af6bfbd507a10c8c4631b50d4b8e7d
kaliztro <69448732+kaliztro@users.noreply.github.com>
2021-11-18 21:22:50 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,80 +1,51 @@
-const Discord = require("discord.js"); 
-const { token } = require("./functions")
-const coisa = require('./config.json')
+const Discord = require("discord.js");
+const client = new Discord.Client({intents: 32767});
+const config = require("./config.json");
 
-//database
-const firebase = require('firebase');
-const firebaseConfig = require('./functions')
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
-//
-
-const client = new Discord.Client()
-
-require('discord-buttons')(client);
-
+client.login(config.token);
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
+client.slashCommands = new Discord.Collection();
 
-client.login(token);
-
-["command", "event"].forEach(handler => {
+//
+["command", "events"].forEach(handler => {
     require(`./handlers/${handler}`)(client);
 });
 
-client.on("message", async message => {
+client.on("ïnteractionCreate", async(interaction) => {
+    if (!interaction.isCommand()) return;
 
-  if (message.guild)
+    const slashcmds = client.slashCommands.get(interaction.commandName)
 
-  database.ref(`Servidor/${message.guild.id}/Prefix`)
+    if (!slashcmds) return;
 
-  .once('value').then(async function (snap) {
-
-    let v = snap.val().Prefix
-  
-    let prefix = v;
-
-    exports.prefix = prefix;
-
-
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    
-    if (cmd.length === 0) return;
-    
-    let command = client.commands.get(cmd); 
-
-    if (!command) command = client.commands.get(client.aliases.get(cmd)); 
-
-    if (command) 
-        command.run(client, message, args, database, firebase, prefix);
-
-    if (!command) return
-    
-    });
-
-    else
-
-  if (!message.guild) {
-
-  if (message.author.bot) return;
-  if (!message.content.startsWith(coisa.prefix)) return;
-
-  const args = message.content.slice(coisa.prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    
-    if (cmd.length === 0) return;
-    
-    let command = client.commands.get(cmd); 
-
-    if (!command) command = client.commands.get(client.aliases.get(cmd)); 
-
-    if (command) 
-        command.run(client, message, args, database, firebase);
-
-    if (!command) message.reply(`o comando "**${message.content}**" nao existe. Digite **${coisa.prefix}ajuda** para ver a lista de comandos 😉 `);
-  }
+    try{
+        await slashcmds.run(client, interaction)
+    } catch (e) {
+        console.error(e)
+    }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+    client.on("ready", async (client, message) => {
+
+    console.log(`Eu estou online agora, meu nome é ${client.user.username}. Há ${client.users.cache.size} usuario(s) em ${client.guilds.cache.size} servidor!`);
+
+    client.user.setPresence({ activities: [{ name: 'Em Manutenção', type: 'LISTENING' }], status: 'idle' });
+
+    });
+//
+
